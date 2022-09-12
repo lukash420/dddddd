8b99a6cedfa9f3117c461de594f7dc568c383bb1
Glitch (glitch-hello-website) <none>
2022-09-12 08:26:47 +0000

    🧀🎄 Checkpoint ./script.js:20359375/1614 ./index.html:20359375/887 ./style.css:20359375/28

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
 
-// Print a message in the browser's dev tools console each time \
-console.log("Hello 🌎");
+console.log("co sie tu patrzysz ?")
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
+
+const btn = document.querySelector("button"); 
 if (btn) {
   btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
+    
     btn.classList.toggle("dipped");
   };
 }
-
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
