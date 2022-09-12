c9eae3a1d9633e78dfb25b09ee260198f602d7eb
Glitch (glitch-hello-website) <none>
2022-09-12 08:15:43 +0000

    👰🗃 Checkpoint ./index.html:20359375/2263 ./script.js:20359375/245

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ This is your site JavaScript code - you can add interactivity and carry out proc
 - Initially the JS writes a message to the console, and moves a button you can add from the README
 */
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+// Print a message in the browser's dev tools console each time \
 console.log("Hello 🌎");
 
 /* 
