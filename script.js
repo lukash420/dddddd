c819b28418b6bb08f91719c008c13abb7a2187fa
Glitch (glitch-hello-website) <none>
2022-10-03 18:34:47 +0000

    👟🌦 Checkpoint ./script.js:20359375/230 ./style.css:20359375/139 ./index.html:20359375/595

--- script.js
+++ script.js
@@ -2,13 +2,4 @@
 console.log("co sie tu patrzysz ?")
 
 
-const btn = document.querySelector("button"); 
-if (btn) {
-  btn.onclick = function() {
-    
-    btn.classList.toggle("dipped");
 
-<input type="button" onclick="location.href='https://google.com';" value="Go to Google" />
-
-     };
-}
