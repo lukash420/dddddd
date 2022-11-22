d85a1c77f1afc1c25cdf6c5486364dc95c6c4200
Glitch (glitch-hello-website) <none>
2022-11-22 19:08:36 +0000

    👱🍶 Checkpoint ./script.js:20359375/174 ./index.html:20359375/34588 ./style.css:20359375/93

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 
 console.log("co sie tu patrzysz ?")
 
-
-    function showP(pic) { 
-        document.getElementById('yby').style.background = 'url(' + 'https://gdybyryby.pl/wp-content/uploads/2019/04/kr%C4%85p.jpg' + ')';
-    };
-
